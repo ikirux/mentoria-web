ae4e7f70fac67ea4827c119689f787fdf0ebb9d2
Carlos Pinto <carlos.pinto@developen.cl>
2021-04-13 16:16:40 -0400

    Se agrega salto de linea

--- code/db-basico.php
+++ code/db-basico.php
@@ -57,6 +57,6 @@ $stmt->execute();
 $users = $stmt->fetchAll(PDO::FETCH_ASSOC);
 
 foreach ($users as $user) {
-    echo $user['id'];
-    echo $user['full_name'];
+    echo $user['id'] . "<br>";
+    echo $user['full_name'] . "<br>";
 }
