6442949c9a6254916eb0ac9a395dba2c61e68581
Carlos Pinto <carlos.pinto@developen.cl>
2021-04-13 15:19:53 -0400

    Se implementa insert de usuarios

--- code/db-basico.php
+++ code/db-basico.php
@@ -10,3 +10,24 @@ try {
 } catch (PDOException $e) {
     echo $e->getMessage();
 }
+
+// Preparar consulta
+$sql = "INSERT INTO users 
+            (full_name, email, user_name, password)
+        VALUES
+            (:full_name, :email, :user_name, :password)";
+
+//statement
+$stmt = $db->prepare($sql);
+
+$full_name = 'Juan Perez';
+$email = "juan.perez@segic.cl";
+$user_name = 'juan.perez';
+$password = 'juan123';
+
+$stmt->bindParam(':full_name', $full_name);
+$stmt->bindParam(':email', $email);
+$stmt->bindParam(':user_name', $user_name);
+$stmt->bindParam(':password', $password);
+
+$stmt->execute();
