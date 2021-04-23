7db178f7236300d378e8abc4a188d6d75c2fba61
Carlos Pinto <carlos.pinto@developen.cl>
2021-04-23 10:33:41 -0400

    Se implementa logout

--- code/main.php
+++ code/main.php
@@ -5,4 +5,17 @@ if (!isset($_SESSION['nombre'])) {
     header("Location: index.php");
 }
 
-echo "Hola, " . $_SESSION['nombre'];
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Main</title>
+</head>
+<body>
+    <?= "Hola, " . $_SESSION['nombre']; ?>
+    <a href="logout.php">(Salir)</a>
+</body>
+</html>
