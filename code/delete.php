b5e149cb1add0e288ca08e9d3f000f12556f3d75
Carlos Pinto <carlos.pinto@developen.cl>
2021-04-27 10:52:11 -0400

    prueba mensaje

--- code/delete.php
+++ code/delete.php
@@ -5,6 +5,6 @@ $id = $_GET["id"];
 // hago las consulas
 
 session_start();
-$_SESSION["msg-delete"] = "El registro se elimino correctamente";
+$_SESSION["msg-delete"] = "El registro se elimino correctamente $id";
 
 header("Location: index.php");
