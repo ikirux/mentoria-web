f8e821ace64b61bf2a11d1772fc3696ed9a80bfe
Carlos Pinto <carlos.pinto@developen.cl>
2021-05-25 10:58:45 -0400

    Se implementa metodo para guardar rutas post

--- code/core/Router.php
+++ code/core/Router.php
@@ -20,6 +20,11 @@ class Router
         $this->routes['get'][$path] = $callback;
     }
 
+    public function post($path, $callback)
+    {
+        $this->routes['post'][$path] = $callback;
+    }
+
     public function resolve()
     {
         /*echo '<pre>';
