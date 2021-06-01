d13496926f7a0ef6a33691832f3a37e865c1b68e
Carlos Pinto <carlos.pinto@developen.cl>
2021-06-01 10:20:06 -0400

    Se agregan return a la acciones de auth

--- code/controllers/AuthController.php
+++ code/controllers/AuthController.php
@@ -9,11 +9,11 @@ class AuthController extends Controller
 {
     public function login()
     {
-        $this->render('login');
+        return $this->render('login');
     }
 
     public function register(Request $request)
     {
-        $this->render('register');
+        return $this->render('register');
     }
 }
