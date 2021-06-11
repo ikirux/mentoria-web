ad65805d6ea7cf5c4f9e2dd0ca5eb9d1bf743fb6
Carlos Pinto <carlos.pinto@developen.cl>
2021-06-11 11:17:33 -0400

    Se corrige nombre field

--- code/views/register.php
+++ code/views/register.php
@@ -2,7 +2,7 @@
 
 <?php $form = \app\core\widgets\Form::begin('', 'POST') ?>
   <?= $form->field($model, 'firstname') ?>
-  <?= $form->field($model, 'lasttname') ?>
+  <?= $form->field($model, 'lastname') ?>
   <?= $form->field($model, 'email') ?>
   <?= $form->field($model, 'password') ?>
   <?= $form->field($model, 'confirmPassword') ?>
