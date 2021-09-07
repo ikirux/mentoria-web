b570cf4f7d958a48ba35421b2786850361317c68
Carlos Pinto <carlos.pinto@developen.cl>
2021-09-07 16:54:14 -0300

    Se prueba el metodo de implementar el metodo getRouteKeyName

--- blog/app/Models/Post.php
+++ blog/app/Models/Post.php
@@ -12,4 +12,9 @@ class Post extends Model
 
     //public $fillable = ['title', 'resumen', 'body'];
     protected $guarded = ['id'];
+
+    public function getRouteKeyName()
+    {
+        return 'slug';
+    }
 }
