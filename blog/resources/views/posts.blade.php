73fccc3532219c37b53c0e4da422ba5be2f28a86
Carlos Pinto <carlos.pinto@developen.cl>
2021-10-05 15:13:19 -0300

    Se comienza a trabajar sobre el listado de posts

--- blog/resources/views/posts.blade.php
+++ blog/resources/views/posts.blade.php
@@ -5,8 +5,9 @@
         <x-post-main :post="$posts[0]" />
 
         <div class="lg:grid lg:grid-cols-2">
-            <x-post-card />
-            <x-post-card />
+            @foreach ($posts->skip(1) as $post) 
+                <x-post-card />
+            @endforeach
         </div>
 
         <div class="lg:grid lg:grid-cols-3">
