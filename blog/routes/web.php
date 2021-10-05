21bd83c19636c85f011e420fd5ffe3c236622374
Carlos Pinto <carlos.pinto@developen.cl>
2021-10-05 15:55:51 -0300

    Debug posts vacios

--- blog/routes/web.php
+++ blog/routes/web.php
@@ -18,9 +18,10 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('posts', [
-        'posts' => Post::latest('published_at')
+        /*'posts' => Post::latest('published_at')
             ->with(['category', 'author'])
-            ->get()
+            ->get()*/
+        'posts' => [],
     ]);
 });
 
