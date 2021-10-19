55dec7e8f19ad1919ac8154a0aaf136165f407d8
Carlos Pinto <carlos.pinto@developen.cl>
2021-10-19 16:01:35 -0300

    Cambios css

--- blog/resources/views/_posts-header.blade.php
+++ blog/resources/views/_posts-header.blade.php
@@ -15,7 +15,7 @@
         <div class="relative flex lg:inline-flex items-center bg-gray-100 rounded-xl">
             <div x-data="{ show: false }" @click.away="show = false">
                 <button @click="show = !show" class="py-2 pl-3 pr-9 text-sm font-semibold lg:w-32 w-full flex lg:inline-flex">
-                    {{ isset($currentCategory) ? $currentCategory->name : 'Categories' }}
+                    {{ isset($currentCategory) ? ucwords($currentCategory->name) : 'Categories' }}
 
                     <svg class="transform -rotate-90 absolute pointer-events-none" style="right: 12px;" width="22" height="22" viewBox="0 0 22 22">
                         <g fill="none" fill-rule="evenodd">
@@ -29,7 +29,7 @@
                 <div x-show="show" class="py-2 absolute bg-gray-100 w-full mt-2 rounded-xl z-50 text-left px-3 text-sm" style="display: none;">
                     @foreach ($categories as $category)
                     <a href="/category/{{ $category->slug }}" class="block text-left px-3 text-sm leading-6 hover:bg-blue-500 hover:text-white">
-                        {{ $category->name }}
+                        {{ ucwords($category->name) }}
                     </a>
                     @endforeach
                 </div>
