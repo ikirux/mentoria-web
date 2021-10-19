f8111961bbf919ecdabb1ffbad183a35eabe4112
Carlos Pinto <carlos.pinto@developen.cl>
2021-10-19 15:28:14 -0300

    Cambios css

--- blog/resources/views/_posts-header.blade.php
+++ blog/resources/views/_posts-header.blade.php
@@ -18,7 +18,7 @@
                     Category
                 </button>
 
-                <div x-show="show" class="py-2 absolute bg-gray-100 w-full">
+                <div x-show="show" class="py-2 absolute bg-gray-100 w-full mt-2">
                     <a href="#" class="block">One</a>
                     <a href="#" class="block">Two</a>
                     <a href="#" class="block">Three</a>
