6bde2a116b501fff5919dcd22ffb697a88d3d9f1
Carlos Pinto <carlos.pinto@developen.cl>
2021-10-22 15:33:51 -0300

    Se agrega componente para los links del dropdown

--- blog/resources/views/_posts-header.blade.php
+++ blog/resources/views/_posts-header.blade.php
@@ -33,11 +33,9 @@
                 </a>
 
                 @foreach ($categories as $category)
-                    <a href="/category/{{ $category->slug }}" 
-                        class="block text-left px-3 text-sm leading-6 hover:bg-blue-500 hover:text-white
-                             {{ isset($currentCategory) && $currentCategory->is($category) ? 'bg-blue-500 text-white' : '' }}">
+                    <x-dropdown-item href="/category/{{ $category->slug }}" :actived="isset($currentCategory) && $currentCategory->is($category)">
                         {{ ucwords($category->name) }}
-                    </a>
+                    </x-dropdown-item>
                 @endforeach
             </x-dropdown>
         </div>
