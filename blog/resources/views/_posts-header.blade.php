abec2cf6dc683556448f8786df10f136f155b178
Carlos Pinto <carlos.pinto@developen.cl>
2021-10-22 15:22:53 -0300

    Se corrige subida de archivos

--- blog/resources/views/_posts-header.blade.php
+++ blog/resources/views/_posts-header.blade.php
@@ -28,8 +28,7 @@
                     </button>
                 </x-slot>
 
-                <a href="/" class="block text-left px-3 text-sm leading-6 hover:bg-blue-500 hover:text-white
-                             {{ isset($currentCategory) && $currentCategory->is($category) ? 'bg-blue-500 text-white' : '' }}">
+                <a href="/" class="block text-left px-3 text-sm leading-6 hover:bg-blue-500 hover:text-white">
                     All
                 </a>
 
