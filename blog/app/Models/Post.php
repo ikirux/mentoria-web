05ec4ad85aa8181d9e4b2b4676f73d7b6c4e480f
Carlos Pinto <carlos.pinto@developen.cl>
2021-10-26 15:50:54 -0300

    Se implementa when en el scope

--- blog/app/Models/Post.php
+++ blog/app/Models/Post.php
@@ -21,10 +21,10 @@ class Post extends Model
 
     public function scopeFilter($query, array $filters)
     {
-        $query->when(
+        return $query->when(
             isset($filters['search']),
             fn ($query, $search) =>
-            $query->where('title', 'like', '%' . $search . '%')
+            $query->where('title', 'like', "%$search%")
                     ->orWhere('resumen', 'like', "%$search%")
         );
 
