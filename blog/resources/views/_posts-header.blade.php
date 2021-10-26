ab68a51a61afa6336684ced5c33386d4f72e78f7
Carlos Pinto <carlos.pinto@developen.cl>
2021-10-26 15:14:23 -0300

    Se guarda la busqueda del filtro en la vista

--- blog/resources/views/_posts-header.blade.php
+++ blog/resources/views/_posts-header.blade.php
@@ -34,7 +34,8 @@
                     type="text" 
                     name="search" 
                     placeholder="Find something" 
-                    class="bg-transparent placeholder-black font-semibold text-sm">
+                    class="bg-transparent placeholder-black font-semibold text-sm"
+                    value="{{ request('search') }}">
             </form>
         </div>
     </div>
