934c51b8416dc769bdad3357c6e7106127e2ccbc
Carlos Pinto <carlos.pinto@developen.cl>
2021-11-11 15:15:50 -0300

    Se implementa metodo paginate en la vista posts

--- blog/app/Http/Controllers/PostController.php
+++ blog/app/Http/Controllers/PostController.php
@@ -10,18 +10,14 @@ class PostController extends Controller
 {
     public function index()
     {
-        return Post::latest('published_at')
-                ->filter(request(['search', 'category']))
-                ->paginate(5);
-
-        // return view('posts', [
-        //     'posts' => Post::latest('published_at')
-        //                 ->filter(request(['search', 'category']))
-        //                 ->get(),
-        //     'categories' => Category::all(),
-        //     'currentCategory' =>
-        //         request('category') !== null ? Category::where('slug', request('category'))->first() : null,
-        // ]);
+        return view('posts', [
+            'posts' => Post::latest('published_at')
+                        ->filter(request(['search', 'category']))
+                        ->paginate(5),
+            'categories' => Category::all(),
+            'currentCategory' =>
+                request('category') !== null ? Category::where('slug', request('category'))->first() : null,
+        ]);
     }
 
     public function show(Post $post)
